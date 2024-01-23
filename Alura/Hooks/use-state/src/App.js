ad99c44b8f3e2dc51c0c710e5f8fbde92c73c930
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-01-23 16:25:39 -0300

    use-state aula finalizada

--- Alura/Hooks/use-state/src/App.js
+++ Alura/Hooks/use-state/src/App.js
@@ -1,20 +1,45 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  const [nome, setNome]= useState('Juju');
+  const [endereco, setEndereco] = useState({});
 
-  function alterarNome() {
-    setNome("Maria Juliana Campos Rodrigues");
+  function manipularEndereco(evento) {
+
+    const cep = evento.target.value
+    // O set é assíncrono, por isso a necessidade de criar a const 'cep';
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8)
+    // obter o cep
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setEndereco((enderecoAntigo) => {
+          return {
+            ...enderecoAntigo,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf,
+          }
+        })
+      })
   }
-    
 
-  return (
-    <div className='app'>
-      <header className='app-header'>
-        <h1>{nome}</h1>
-        <button onClick={alterarNome}>Mudar para nome completo</button>
+   return (
+    <div className='App'>
+      <header className=  'App-header'>
+        <input type='number' placeholder='Digite o cep' onChange={manipularEndereco} />
+        <ul>
+          <li>CEP: {endereco.cep}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
       </header>
     </div>
   );
