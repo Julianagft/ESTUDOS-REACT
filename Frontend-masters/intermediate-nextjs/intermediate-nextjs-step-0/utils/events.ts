de6e0eb858ce05f4299aacd5ce7f6028fa90ec98
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-08-24 16:44:47 -0300

    advanced next.js course finished

--- Frontend-masters/intermediate-nextjs/intermediate-nextjs-step-0/utils/events.ts
+++ Frontend-masters/intermediate-nextjs/intermediate-nextjs-step-0/utils/events.ts
@@ -28,8 +28,39 @@ export const getEventsForDashboard = memoize (async (userId: string) => {
 },
 {
     persist: true,
-    revalidateTags: (userId, eventId) => ['event', eventId],
+    revalidateTags: (userId) => ['event', userId],
     suppressWarnings: true,
     logid: 'event',
 
 });
+
+export const getAllEvents = memoize(
+    async (userId: string) => {
+      await delay()
+      return db.query.events.findMany({
+        where: eq(events.createdById, userId),
+        orderBy: [asc(events.startOn)],
+      })
+    },
+    {
+      persist: true,
+      revalidateTags: () => ['events'],
+      suppressWarnings: true,
+      logid: 'events',
+    }
+  )
+
+  export const getOneEvent = memoize(
+    async (userId: string, eventId: string) => {
+      await delay()
+      return db.query.events.findFirst({
+        where: and(eq(events.createdById, userId), eq(events.id, eventId)),
+      })
+    },
+    {
+      persist: true,
+      revalidateTags: (userId, eventId) => ['event', eventId],
+      suppressWarnings: true,
+      logid: 'event',
+    }
+  )
