5358a0ce870acc71e60cc7afd61e51e63fb50c16
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-11-28 21:56:47 -0300

    subscribe and binding

--- Frontend-masters/redux/redux-without-react/index.js
+++ Frontend-masters/redux/redux-without-react/index.js
@@ -1,14 +1,46 @@
 import {
-    createStore,
     compose,
     applyMiddleware,
     bindActionCreators,
 } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 
-const makeLouder = string => string.toUpperCase();
-const repeatThreeTimes = string => string.repeat(3);
-const embolden = string => string.bold();
+const initialState = { value: 0 };
 
-const makeLouderAndRepeatThreeTimes = string => repeatThreeTimes(makeLouder(string));
+const INCREMENT = 'INCREMENT';
+const ADD = 'ADD';
 
-console.log(makeLouderAndRepeatThreeTimes('hello')); 
+const incrementAction = { type: INCREMENT };
+
+const increment = () => ({ type: INCREMENT});
+const add = (amount) => ({ type: ADD, payload: amount });
+
+const reducer = (state = initialState, action) => {
+    if (action.type === INCREMENT) {
+        return { value: state.value + 1 };
+    };
+
+    if (action.type === ADD) {
+        return { value: state.value + action.payload };
+    };
+
+    return state;
+};
+
+const store = createStore(reducer, initialState);
+
+const subscriber = () => console.log('SUBSCRIBER', store.getState());
+
+const actions = bindActionCreators({ increment, add }, store.dispatch);
+// O primeiro argumento é o objeto com todos as diferentes actionns creators que queremos unir e o segundo é AO QUE queremos uni-los. 
+
+const [dispatchIncrement, dispatchAdd] = [increment, add].map((fn) => compose(store.dispatch, fn));
+
+console.log("actions: ", actions);
+// store.subscribe(subscriber);
+// store.dispatch(increment());
+// store.dispatch(add(1000));
+actions.add(1000);
+actions.increment();
+
+console.log(store.getState());
