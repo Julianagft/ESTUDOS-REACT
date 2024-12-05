15490ac4e3571102330d59d5dd7e9f4de719c242
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-12-04 22:06:02 -0300

    middleware solution

--- Frontend-masters/redux/redux-without-react/index4.js
+++ Frontend-masters/redux/redux-without-react/index4.js
@@ -10,7 +10,7 @@ const reducer = (state = {count: 1}) => state;
 
 
 const monitorEnhancer = (createStore) => (reducer, initialState, enhancer) => {
-    const monitorReducer = (state, action) => {
+        const monitorReducer = (state, action) => {
         const start = performance.now();
 
         const newState = reducer(state, action);
@@ -38,6 +38,21 @@ const logEnhancer = (createStore) => (reducer, initialState, enhancer) => {
     return createStore(logReducer, initialState, enhancer);
 };
 
-const store = createStore(reducer, compose(logEnhancer, monitorEnhancer)); // O único argumento obrigatório é a função do reducer. Todo o resto é opcional.
+const logMiddleware = (store) => (next) => (action) => {
+    console.log('Old State:', store.getState(),  action);
+    next(action);
+    console.log('new state:', store.getState(), action);
+}
+
+const monitorMiddleware = (store) => (next) => (action) => {
+    // Pra lembrar lembra de SNACK (store, next, action)
+    const start = performance.now();
+    next(action);
+    const end = performance.now();
+    const diff = end - start;
+    console.log(diff);
+}
+
+const store = createStore(reducer, applyMiddleware(logMiddleware, monitorMiddleware)); 
 
 store.dispatch({ type: 'ADD' });
